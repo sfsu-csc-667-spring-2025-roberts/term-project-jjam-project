3820c35d9ebff090a28f2df4e13be653de523261
Doiboy35 <justinrdoi@gmail.com>
2025-04-27 01:49:11 -0700

    can create games, with some caveats

--- src/server/db/games/index.ts
+++ src/server/db/games/index.ts
@@ -1,14 +1,20 @@
 import db from "../connection";
+import { ADD_PLAYER, CONDITIONALLY_JOIN_SQL, CREATE_SQL } from "./sql";
 
-const CREATE_SQL = `INSERT INTO games (name, min_players, max_players, password) VALUES ($1, $2, $3, $4) RETURNING id`;
-const ADD_PLAYER = `INSERT INTO game_users (game_id, user_id) VALUES ($1, $2)`;
+// const CREATE_SQL = `INSERT INTO games (name, min_players, max_players, password) VALUES ($1, $2, $3, $4) RETURNING id`;
+// const ADD_PLAYER = `INSERT INTO game_users (game_id, user_id) VALUES ($1, $2)`;
 
 const create = async (name: string, minPlayers: string, maxPlayers: string, password: string, userId: number) => {
-    const { id: gameId } = await db.one(CREATE_SQL, [name, minPlayers, maxPlayers, password,]);
+    const { id: gameId } = await db.one<{id: number}>(CREATE_SQL, [name, minPlayers, maxPlayers, password,]);
 
     await db.none(ADD_PLAYER, [gameId, userId]);
 
     return gameId;
 }
 
-export default { create }
+const join = async (userId: number, gameId: number, password: string = "") =>{
+    const { playerCount } = await db.one(CONDITIONALLY_JOIN_SQL, {gameId, userId, password});
+    return playerCount;
+}
+
+export default { create, join }
